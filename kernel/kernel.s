da3883a6260761d78c8b473ea1aac6382c087e69
trendavi <dvid.trent@gmail.com>
2022-10-01 08:09:21 -0500

    unused imports and format

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -1,18 +1,4 @@
 .export kinit
 
-.import PORTB
-.import PORTA
-.import DDRB
-.import DDRA
- 
-.import PCR
-.import IFR
-.import IER
-
-.import E
-.import RW
-.import RS
-
-
 kinit:
-    jmp kinit
+          JMP kinit
